b1377c28433bff352ff7f73fda8954e205b8e3e8
Niklaus Giger <niklaus.giger@member.fsf.org>
2014-04-23 09:41:13 +0200

    Added test target to rake

---
+++ test_options.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+# Helper script to test all common usageas of oddb2xml
+# - runs rake install to install the gem
+# - Creates an output directory ausgabe/time_stamp
+# - runs all commands (and add ---skip-download)
+# - saveds output and data/download to ausgabe/time_stamp
+
+
+require 'fileutils'
+
+def test_one_call(cmd)
+  dest = File.join(Ausgabe, cmd.gsub(/[ -]/, '_'))
+  cmd.sub!('oddb2xml',  'oddb2xml --skip-download')
+  files = (Dir.glob('%.xls*') + Dir.glob('*.dat*') + Dir.glob('*.xml'))
+  FileUtils.rm(files, :verbose => true)
+  puts "#{Time.now}: Running cmd #{cmd}"
+  startTime = Time.now
+  res = system(cmd)
+  endTime = Time.now
+  diffSeconds = (endTime - startTime).to_i
+  duration = "#{Time.now}: Took #{sprintf('%3d', diffSeconds)} seconds for"
+  puts "#{duration} success #{res} for #{cmd}"
+  exit 2 unless res
+  FileUtils.makedirs(dest)
+  FileUtils.cp_r('data/download', dest, :preserve => true, :verbose => true) if Dir.glob(Ausgabe).size > 0
+  FileUtils.cp(Dir.glob('*.dat'), dest, :preserve => true, :verbose => true) if Dir.glob('*.dat').size > 0
+  FileUtils.cp(Dir.glob('*.xml'), dest, :preserve => true, :verbose => true) if Dir.glob('*.xml').size > 0
+  FileUtils.cp(Dir.glob('*.gz'),  dest, :preserve => true, :verbose => true) if Dir.glob('*.gz').size > 0
+end
+
+
+system("rake install") # build and install our gem first
+Ausgabe = File.join(Dir.pwd, 'ausgabe', Time.now.strftime('%Y.%m.%d-%H:%M'))
+FileUtils.makedirs(Ausgabe)
+test_one_call('oddb2xml -f dat', 'oddb.dat')
+test_one_call('oddb2xml -f xml', 'oddb.xml')
+test_one_call('oddb2xml -f dat -a nonpharma')
+test_one_call('oddb2xml -t md')
+test_one_call('oddb2xml -a nonpharma -t md -c tar.gz')
+test_one_call('oddb2xml -e') # take hours, therefor at the end
