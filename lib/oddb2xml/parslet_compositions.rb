4a64da514c1831d14b5b02366be2bb616669c873
Niklaus Giger <ngiger@ywesee.com>
2015-04-08 17:41:22 +0200

    Parsing simple compositions

--- lib/oddb2xml/parslet_compositions.rb
+++ lib/oddb2xml/parslet_compositions.rb
@@ -8,8 +8,7 @@
 
 require 'parslet'
 include Parslet
-
-ParseComposition   = Struct.new("ParseComposition",  :source, :label, :label_description, :substances, :galenic_form, :route_of_administration)
+VERBOSE_MESSAGES = false
 
 class DoseParser < Parslet::Parser
 
@@ -43,29 +42,14 @@ class DoseParser < Parslet::Parser
                            str('g') |
                            str('l') |
                            str('ml') |
-                           str('mg/ml')).as(:unit) }
+                           str('mg/ml') |
+                           str('U.I.')
+                          ).as(:unit) }
   rule(:qty_unit)       { dose_qty >> space? >> dose_unit }
   rule(:dose_qty)       { number.as(:qty) }
   rule(:dose)           { qty_unit | dose_qty |dose_unit}
   root :dose
 
-end
-
-class SubstanceParser < DoseParser
-
-  rule(:operator)   { match('[+]') >> space? }
-
-  # Grammar parts
-  rule(:sum)        {
-    number.as(:left) >> operator.as(:op) >> expression.as(:right) }
-  rule(:arglist)    { expression >> (comma >> expression).repeat }
-#  rule(:funcall)    {
-#    identifier.as(:funcall) >> lparen >> arglist.as(:arglist) >> rparen }
-
-  rule(:substance_name) { identifier.as(:substance_name) >> dose.repeat(0,1) }
-  rule(:expression) { substance_name }
-  #rule(:expression) { dose | funcall | sum | integer }
-  root :expression
 end
 
 class IntLit   < Struct.new(:int)
@@ -83,7 +67,7 @@ class FunCall < Struct.new(:name, :args);
   end
 end
 
-class CompositionsTransformer < Parslet::Transform
+class DoseTransformer < Parslet::Transform
   rule(:int => simple(:int))        { IntLit.new(int) }
   rule(:number => simple(:nb)) {
     nb.match(/[eE\.]/) ? Float(nb) : Integer(nb)
@@ -104,17 +88,63 @@ class CompositionsTransformer < Parslet::Transform
     :unit    => simple(:unit))  { ParseDose.new(nil, unit) }
   rule(
     :qty    => simple(:qty))  { ParseDose.new(qty, nil) }
+end
 
-  rule( :substance_name => simple(:substance_name) )  {
-    ParseSubstance.new(substance_name)
+
+class SubstanceParser < DoseParser
+
+  rule(:operator)   { match('[+]') >> space? }
+
+  # Grammar parts
+  rule(:sum)        {
+    number.as(:left) >> operator.as(:op) >> expression.as(:right) }
+  rule(:arglist)    { expression >> (comma >> expression).repeat }
+#  rule(:funcall)    {
+#    identifier.as(:funcall) >> lparen >> arglist.as(:arglist) >> rparen }
+
+  rule(:substance) { (identifier >> space.maybe).repeat.as(:substance) >> str('.').maybe >> dose.repeat(0,1) }
+  rule(:excipiens) { str('excipiens') >> space >> (str('pro') >> space).maybe >> substance }
+  rule(:substances) { substance.repeat(0, 1) >> additional_substance.repeat(0) }
+  rule(:additional_substance) { str(',') >> space >> substance }
+
+  rule(:expression) { excipiens | substances }
+  #rule(:expression) { dose | funcall | sum | integer }
+  root :expression
+end
+
+class SubstanceTransformer < DoseTransformer
+  rule(:substance => simple(:substance)) {
+    |dictionary|
+    ParseSubstance.new(dictionary[:substance])
+  }
+end
+
+class CompositionParser < SubstanceParser
+
+  rule(:composition) { (substances >> str('.').repeat(0,2)) }
+  rule(:expression) { composition }
+  root :expression
+end
+
+class CompositionTransformer < DoseTransformer
+  rule( :substance => simple(:substance) )  {
+    |dictionary|
+    @@curSubstance = ParseSubstance.new(dictionary[:substance])
   }
 
+  rule(
+    :qty    => simple(:qty),
+    :unit   => simple(:unit))  {
+    |dictionary|
+      @@curSubstance.dose = ParseDose.new(dictionary[:qty], dictionary[:unit])
+      nil
+    }
 end
 
 class ParseDose
   attr_reader :qty, :unit
   def initialize(qty=nil, unit=nil)
-    puts "ParseDose.new from #{qty.inspect} #{unit.inspect} #{unit.inspect}" if $VERBOSE
+    puts "ParseDose.new from #{qty.inspect} #{unit.inspect} #{unit.inspect}" if VERBOSE_MESSAGES
     if qty and (qty.is_a?(String) || qty.is_a?(Parslet::Slice))
       @qty  = qty.to_s.index('.') ? qty.to_s.to_f : qty.to_s.to_i
     elsif qty
@@ -130,7 +160,7 @@ class ParseDose
   def ParseDose.from_string(string)
     value = nil
     parser = DoseParser.new
-    transf = CompositionsTransformer.new
+    transf = DoseTransformer.new
     result = transf.apply(parser.parse(string))
   end
 end
@@ -138,34 +168,64 @@ end
 class ParseSubstance
   attr_accessor  :name, :qty, :unit, :chemical_substance, :chemical_qty, :chemical_unit, :is_active_agent, :dose, :cdose
   def initialize(name, dose=nil)
-    puts "ParseSubstance.new from #{name.inspect} #{dose.inspect}" if $VERBOSE
-    @name = name.to_s.capitalize.strip
+    puts "ParseSubstance.new from #{name.inspect} #{dose.inspect}" if VERBOSE_MESSAGES
+    @name = name.to_s.split(/\s/).collect{ |x| x.capitalize }.join(' ').strip
     if dose
       @qty = dose.qty
       @unit = dose.unit
     end
+  end
+  def qty
+    @dose ? @dose.qty : @qty
+  end
+  def unit
+    @dose ? @dose.unit : @unit
   end
   def eval
     self
   end
   def ParseSubstance.from_string(string)
     value = nil
-    puts "ParseSubstance for string #{string})}" if $VERBOSE
+    puts "ParseSubstance for string #{string}" if VERBOSE_MESSAGES
     parser = SubstanceParser.new
-    transf = CompositionsTransformer.new
+    transf = SubstanceTransformer.new
     result = transf.apply(parser.parse(string))
     if result.is_a?(Array)
-      second = result[0]
+      second = result.flatten[0]
       if second.name.eql?('E') # Handle Farbstoffe
         second.name = "E #{result[1].qty}"
       else
-        second.dose = result[1]
-        second.qty = result[1].qty
-        second.unit = result[1].unit
+        if result.size > 1
+          second.dose = result[1]
+          second.qty  = result[1].qty
+          second.unit = result[1].unit
+          puts "ParseSubstance returning second #{second}" if VERBOSE_MESSAGES
+        end
       end
       return second
     end
     result
+  end
+end
+
+class ParseComposition
+  attr_accessor   :source, :label, :label_description, :substances, :galenic_form, :route_of_administration
+  def initialize(source)
+    @substances ||= []
+    puts "ParseComposition.new from #{source.inspect} @substances #{@substances.inspect}" if VERBOSE_MESSAGES
+    @source = source.to_s
+  end
+  def eval
+    self
+  end
+  def ParseComposition.from_string(string)
+    value = nil
+    result = ParseComposition.new(string)
+    parser3 = CompositionParser.new
+    transf3 = CompositionTransformer.new
+    ast = transf3.apply(parser3.parse(string))
+    ast.find_all{|x| x.is_a?(ParseSubstance)}.each{ |x| result.substances << x }
+    return result
   end
 end
 
--- spec/parslet_spec.rb
+++ spec/parslet_spec.rb
@@ -71,9 +71,26 @@ describe ParseDose do
   end
 end if RunAllTests
 
+
+describe ParseSubstance do
+  context "should return correct substance for 'pyrazinamidum'" do
+    string = "pyrazinamidum"
+    substance = ParseSubstance.from_string(string)
+    specify { expect(substance.name).to eq 'Pyrazinamidum' }
+    specify { expect(substance.qty).to eq nil }
+    specify { expect(substance.unit).to eq nil }
+  end
+  context "should return correct substance for 'E 120'" do
+    string = "E 120"
+    substance = ParseSubstance.from_string(string)
+    specify { expect(substance.name).to eq string }
+    specify { expect(substance.qty).to eq nil }
+    specify { expect(substance.unit).to eq nil }
+  end
+end
+
 describe ParseSubstance do
 # ParseSubstance     = Struct.new("ParseSubstance",    :name, :qty, :unit, :chemical_substance, :chemical_qty, :chemical_unit, :is_active_agent, :dose, :cdose)
-
   context "should return correct substance for 'pyrazinamidum'" do
     string = "pyrazinamidum"
     substance = ParseSubstance.from_string(string)
@@ -98,4 +115,104 @@ describe ParseSubstance do
     specify { expect(substance.unit).to eq 'mg' }
   end
 
+  context "should return correct substance for 'excipiens pro compresso.'" do
+    string = "excipiens pro compresso."
+    substance = ParseSubstance.from_string(string)
+    specify { expect(substance.name).to eq 'Compresso' }
+    specify { expect(substance.qty).to eq nil}
+    specify { expect(substance.unit).to eq nil }
+  end
+
+  context "should return correct substance for 'retinoli palmitas 7900 U.I.'" do
+    string = "retinoli palmitas 7900 U.I."
+    substance = ParseSubstance.from_string(string)
+    specify { expect(substance.name).to eq 'Retinoli Palmitas' }
+    specify { expect(substance.qty).to eq 7900.0}
+    specify { expect(substance.unit).to eq 'U.I.' }
+  end
+end if RunAllTests
+
+describe ParseComposition do
+# ParseComposition   = Struct.new("ParseComposition",  :source, :label, :label_description, :substances, :galenic_form, :route_of_administration)
+
+  context "should return correct composition for 'terra'" do
+    source = 'terra'
+    composition = ParseComposition.from_string(source)
+    specify { expect(composition.source).to eq source }
+    specify { expect(composition.label).to eq nil }
+    specify { expect(composition.label_description).to eq nil }
+    specify { expect(composition.galenic_form).to eq nil }
+    specify { expect(composition.route_of_administration).to eq nil }
+    specify { expect( composition.substances.first.name).to eq "Terra" }
+  end
+
+  context "should return correct composition for 'terra silicea spec..'" do
+    source = 'terra silicea spec..'
+    composition = ParseComposition.from_string(source)
+    specify { expect(composition.source).to eq source }
+    specify { expect(composition.label).to eq nil }
+    specify { expect(composition.label_description).to eq nil }
+    specify { expect(composition.galenic_form).to eq nil }
+    specify { expect(composition.route_of_administration).to eq nil }
+    specify { expect( composition.substances.first.name).to eq "Terra Silicea Spec" }
+  end
+
+  context "should return correct composition for 'minoxidilum'" do
+    source = 'minoxidilum 2.5 mg'
+    composition = ParseComposition.from_string(source)
+    specify { expect(composition.source).to eq source }
+    specify { expect(composition.label).to eq nil }
+    specify { expect(composition.label_description).to eq nil }
+    specify { expect(composition.galenic_form).to eq nil }
+    specify { expect(composition.route_of_administration).to eq nil }
+    substance = composition.substances.first
+    specify { expect(substance.name).to eq 'Minoxidilum' }
+    specify { expect(substance.qty).to eq 2.5 }
+    specify { expect(substance.unit).to eq 'mg' }
+  end
+
+  context "should return correct composition for 'minoxidilum'" do
+    source = 'minoxidilum'
+    composition = ParseComposition.from_string(source)
+    specify { expect(composition.source).to eq source }
+    specify { expect(composition.label).to eq nil }
+    specify { expect(composition.label_description).to eq nil }
+    specify { expect(composition.galenic_form).to eq nil }
+    specify { expect(composition.route_of_administration).to eq nil }
+    substance = composition.substances.first
+    specify { expect(substance.name).to eq 'Minoxidilum' }
+    specify { expect(substance.qty).to eq nil }
+    specify { expect(substance.unit).to eq nil }
+  end
+
+  context "should return correct composition for 'minoxidilum'" do
+    source = 'minoxidilum 2.5 mg, pyrazinamidum 500 mg'
+    composition = ParseComposition.from_string(source)
+    specify { expect(composition.source).to eq source }
+    specify { expect(composition.label).to eq nil }
+    specify { expect(composition.label_description).to eq nil }
+    specify { expect(composition.galenic_form).to eq nil }
+    specify { expect(composition.route_of_administration).to eq nil }
+    substance = composition.substances.first
+    specify { expect(substance.name).to eq 'Minoxidilum' }
+    specify { expect(substance.qty).to eq 2.5 }
+    specify { expect(substance.unit).to eq 'mg' }
+  end
+
+  context "should return correct composition for 'minoxidilum excipiens'" do
+    source = 'minoxidilum 2.5 mg, excipiens pro compresso.'
+    composition = ParseComposition.from_string(source)
+    specify { expect(composition.source).to eq source }
+    specify { expect(composition.label).to eq nil }
+    specify { expect(composition.label_description).to eq nil }
+    specify { expect(composition.galenic_form).to eq nil }
+    specify { expect(composition.route_of_administration).to eq nil }
+
+    substance = composition.substances.first
+    specify { expect(substance.name).to eq 'Minoxidilum' }
+    specify { expect(substance.qty).to eq 2.5 }
+    specify { expect(substance.unit).to eq 'mg' }
+    specify { expect(composition.substances.last.name).to eq 'Excipiens Pro Compresso' }
+  end
 end
+
