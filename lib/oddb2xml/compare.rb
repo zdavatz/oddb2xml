7093ff40d578aa6ec8564d724f3481758fdd62b0
Niklaus Giger <niklaus.giger@member.fsf.org>
2017-12-14 09:55:48 +0100

    Compare v3/v5 and refactoring

--- lib/oddb2xml/compare.rb
+++ lib/oddb2xml/compare.rb
@@ -2,6 +2,69 @@
 require 'xmlsimple'
 
 module Oddb2xml
+  class StammXML
+    V3_NAME_REG = /_([N,P])_/
+    attr_accessor :components
+    attr_reader :keys, :sub_key_names, :filename, :basename, :version, :hash
+    def initialize(filename, components =  ['ITEMS'])
+      raise "File #{filename} must exist" unless File.exist?(filename)
+      @filename = filename
+      @basename = File.basename(filename)
+      @version =  V3_NAME_REG.match(filename) ? 3 : 5
+      @components = components
+      if @version == 5
+        @hash = load_file(@filename)
+      else
+        [ @filename.sub(V3_NAME_REG, '_P_'),
+          @filename.sub(V3_NAME_REG, '_N_'),
+        ].each do |fname|
+          raise "File #{filename} must exist" unless File.exist?(filename)
+          @hash = load_file(fname)
+          version_from_xml = hash['DATA_QUALITY'].to_i
+          items = @hash['ITEM']
+          items.each { |item| item['PHARMATYPE'] =  V3_NAME_REG.match(fname)[1]}
+          @sub_key_names = items.collect{|item| item.keys}.flatten.uniq
+          @keys  = items.collect{|item| item['GTIN'].first.to_i }
+          if V3_NAME_REG.match(fname)[0].eql?('_P_')
+            @p_items = items
+          else
+            @n_items = items
+          end
+          raise "Unexpected version #{version_from_xml} in #{fname}" unless version_from_xml == 3
+        end
+        @hash['ITEMS'] = @p_items + @n_items
+        puts "#{Time.now.strftime("%H:%M:%S")}: V3 #{filename} has #{@p_items.size} Pharma and #{@n_items.size} NonPharma items"
+      end
+    end
+    def self.get_component_key_name(component_name)
+        return 'LIMNAMEBAG' if /LIMITATION/i.match(component_name)
+        return 'PRODNO' if /PRODUCT/i.match(component_name)
+        return 'GTIN' if /ITEM/i.match(component_name)
+        raise "Cannot determine keyname for component #{component_name}"
+    end
+    def get_limitation_from_v5(item)
+      get_item('PRODUCTS', item['PRODNO'].first.to_i)['LIMNAMEBAG'] ? ['true'] : nil
+    end
+    def get_field_from_v5_product(item, field_name)
+      get_item('PRODUCTS', item['PRODNO'].first.to_i)[field_name]
+    end
+    def get_items(component_name)
+      if @version == 3
+        items = @hash[component_name]
+      else
+        items = @hash[component_name].first.values.first
+      end
+      items
+    end
+    def get_item(component_name, id)
+      keyname = StammXML.get_component_key_name(component_name)
+      get_items(component_name).find{|item| item[keyname].first.to_i == id}
+    end
+    def load_file(name)
+      puts "#{Time.now.strftime("%H:%M:%S")}: Reading #{name} #{(File.size(name)/1024/1024).to_i} MB. This may take some time"
+      XmlSimple.xml_in(IO.read(name))
+    end
+  end
   class CompareV5
     DEFAULTS = {
       :components => ["PRODUCTS", "LIMITATIONS", "ITEMS",],
@@ -9,75 +72,70 @@ module Oddb2xml
       :fields_as_floats => [ 'PEXT', 'PEXF', 'PPUB' ],
       :min_diff_for_floats => 0.01,
     }
-    def initialize(left, right)
-      raise "File #{left} must exist" unless File.exist?(left)
-      raise "File #{right} must exist" unless File.exist?(right)
-      @left = left
-      @right = right
-      @l_base = File.basename(@left)
-      @r_base = File.basename(@right)
+    def initialize(left, right, options = DEFAULTS.clone)
+      @options = options
+      @left = StammXML.new(left, @options[:components])
+      @right = StammXML.new(right, @options[:components])
       @diff_stat = {}
+      @occurrences = {}
       @report = []
     end
-    def compare(options = DEFAULTS)
-      show_header("Start comparing #{@left} with #{@righ}t")
-      @options = options
-      @l_hash = load_file(@left)
-      @r_hash = load_file(@right)
-      if @l_hash['DATA_QUALITY'].eql?('3') ||  @r_hash['DATA_QUALITY'].eql?('3')
-        components = ['ITEMS']
-      else
-        components = @options[:components]
-      end
-      components.each do |name|
+    def get_keys(items, key='GTIN')
+      items.collect{|item| item[key].first.to_i }
+    end
+    def get_names(items)
+       items.collect{|item| item.keys}.flatten.uniq.sort
+    end
+    def compare
+      show_header("Start comparing #{@left.filename} with #{@right.filename}")
+      @comp_v3_with_v5 = !!(@left.version != @right.version)
+      @options[:fields_to_ignore] +=  ['DEDUCTIBLE', 'MEASURE', 'MEASUREF',
+                                       'DOSAGE_FORM', 'DOSAGE_FORMF'] if @comp_v3_with_v5
+      (@left.components & @right.components).each do |name|
         begin
+          puts "\n#{Time.now.strftime("%H:%M:%S")}: Comparing #{name} in #{@left.basename} with #{@right.basename}"
+          key = StammXML.get_component_key_name(name)
+          left_items = @left.get_items(name)
+          next unless left_items
+          right_items = @right.get_items(name)
+          next unless right_items
           @diff_stat[name] = {}
+          @occurrences[name] = {}
           @diff_stat[name][NR_COMPARED] = 0
-          puts "\nComparing #{name} in #{@l_base} with #{@r_base}"
-          offset = 0
-          offset = 1 if name.eql?('ITEMS')
-          l_name = r_name = name
-          if @r_hash['DATA_QUALITY'].eql?('3')
-            r_name  = 'ITEM'
-            r_names = @r_hash[r_name].collect{|item| item.keys}.flatten.uniq
-            r_keys  = @r_hash[r_name].collect{|item| item['GTIN'].first.to_i }
-          else
-            r_keys  = @r_hash[r_name].first.values.first.collect{|item| item.values[offset].first.to_i}
-            r_names = @r_hash[r_name].first.values.first.collect{|item| item.keys.first}.uniq
-          end
-          if @l_hash['DATA_QUALITY'].eql?('3')
-            l_name  = 'ITEM'
-            l_names = @l_hash[l_name].collect{|item| item.keys}.flatten.uniq
-            l_keys  = @l_hash[l_name].collect{|item| item['GTIN'].first.to_i }
-          else
-            l_keys  = @l_hash[name].first.values.first.collect{|item| item.values[offset].first.to_i}
-            l_names = @l_hash[l_name].first.values.first.collect{|item| item.keys.first}.uniq
-          end
+          l_names = get_names(left_items)
+          r_names = get_names(right_items)
           compare_names = l_names & r_names
-          compare_details(l_name, r_name, l_keys, r_keys, offset)
-          key_results_details(l_name, l_keys, r_keys, compare_names)
+          l_keys = get_keys(left_items, key)
+          r_keys = get_keys(right_items, key)
+          (l_keys & r_keys).each do |id|
+            compare_details(name, compare_names, id)
+          end
+          key_results_details(name, compare_names, l_keys, r_keys)
         rescue => error
           puts "Execution failed with #{error}"
           binding.pry  if defined?(RSpec)
         end
       end
-      show_header("Summary comparing #{@left} with #{@right}")
+      show_header("#{Time.now.strftime("%H:%M:%S")}: Summary comparing #{@left.filename} with #{@right.filename}")
       puts "Ignored differences in #{@options[:fields_to_ignore]}. Signaled when differences in #{@options[:fields_as_floats]} were bigger than #{@options[:min_diff_for_floats]}"
       puts @report.join("\n")
       @diff_stat.each do |component, stats|
         puts "\nFor #{stats[NR_COMPARED]} #{component} we have the following number of differences per field"
         stats.each do |name, nr|
           next if name.eql?(NR_COMPARED)
-          puts "   #{name.ljust(20)} #{nr}"
+          next if @options[:fields_to_ignore].index(name)
+          puts "   #{name.ljust(20)} #{nr} of #{@occurrences[component][name]}"
         end
       end
       @diff_stat
     rescue => error
       puts "Execution failed with #{error}"
-      binding.pry if defined?(RSpec)
+      # binding.pry if defined?(RSpec)
+      raise error
     end
     private
     NR_COMPARED = 'NR_COMPARED'
+    COUNT       = '_count'
     def show_header(header)
       pad = 5
       puts
@@ -86,61 +144,71 @@ module Oddb2xml
       puts '-'*(header.length+2*pad)
       puts
     end
-    def compare_details(l_name, r_name, l_keys, r_keys, offset)
+    def compare_details(component_name, compare_names, id)
+      l_item = @left.get_item(component_name, id)
+      r_item = @right.get_item(component_name, id)
       found_one = false
       length = 32
-      (l_keys & r_keys).each do |key|
-        if @l_hash['DATA_QUALITY'].eql?('3')
-          right = @r_hash[r_name].find{|item| item['GTIN'].first.to_i == key}
-        else
-          right = @r_hash[r_name].first.values.first.find{|item| item.values[offset].first.to_i == key}
+      found = false
+      detail_name = l_item['DSCR'] ? l_item['DSCR'].first[0..length-1].rjust(length) : ''.rjust(length)
+      details = "Diff in #{id.to_s.ljust(15)} #{detail_name}"
+      diff_name = component_name
+      diff_name += 'S' unless /S$/.match(diff_name)
+      @diff_stat[diff_name] ||= {}
+      @occurrences[diff_name] ||= {}
+      @diff_stat[diff_name][NR_COMPARED] ||= 0
+      @diff_stat[diff_name][NR_COMPARED] += 1
+      l_item.keys.each do |sub_key|
+        next if @options[:fields_to_ignore].index(sub_key)
+        @diff_stat[diff_name][sub_key] ||= 0
+        @occurrences[diff_name][sub_key] ||= 0
+        @occurrences[diff_name][sub_key] += 1
+        r_value = r_item[sub_key]
+        l_value = l_item[sub_key]
+        if @options[:fields_as_floats].index(sub_key)
+          l_float = l_value ? l_value.first.to_f : 0.0
+          r_float = r_value ? r_value.first.to_f : 0.0
+          next if (l_float - r_float).abs < @options[:min_diff_for_floats]
         end
-        if @l_hash['DATA_QUALITY'].eql?('3')
-          left = @l_hash[l_name].find{|item| item['GTIN'].first.to_i == key}
-        else
-          left =  @l_hash[l_name].first.values.first.find{|item| item.values[offset].first.to_i == key}
-        end
-        found = false
-        detail_name = left['DSCR'] ? left['DSCR'].first[0..length-1].rjust(length) : ''.rjust(length)
-        details = "Diff in #{key.to_s.ljust(15)} #{detail_name}"
-        diff_name = l_name
-        diff_name += 'S' unless /S$/.match(diff_name)
-        @diff_stat[diff_name] ||= {}
-        @diff_stat[diff_name][NR_COMPARED] ||= 0
-        @diff_stat[diff_name][NR_COMPARED] += 1
-        left.keys.each do |sub_key|
-          @diff_stat[diff_name][sub_key] ||= 0
-          next if @options[:fields_to_ignore].index(sub_key)
-          if @options[:fields_as_floats].index(sub_key)
-            l_float = left[sub_key] ? left[sub_key].first.to_f : 0.0
-            r_float = right[sub_key] ? right[sub_key].first.to_f : 0.0
-            next if (l_float - r_float).abs < @options[:min_diff_for_floats]
+        if @comp_v3_with_v5
+          if ['ATC', 'LIMITATION', 'LIMITATION_PTS', 'LIMITATION_TEXT', 'LIMNAMEBAG'].index(sub_key)
+            if @right.version == 5
+              if sub_key.eql?('LIMITATION')
+                r_value = @right.get_limitation_from_v5(r_item)
+              else
+                r_value = @right.get_field_from_v5_product(r_item, sub_key)
+              end
+            else
+              if sub_key.eql?('LIMITATION')
+                l_value = @left.get_limitation_from_v5(r_item)
+              else
+                l_value = @left.get_field_from_v5_product(r_item, sub_key)
+              end
+            end
           end
-          next if (right[sub_key].is_a?(Array) && '--missing--'.eql?(right[sub_key].first)) || (left[sub_key].is_a?(Array) && '--missing--'.eql?(left[sub_key].first))
-          next if right[sub_key].to_s.eql?(left[sub_key].to_s)
-          next if right[sub_key].to_s.upcase.eql?(left[sub_key].to_s.upcase) && @options[:case_insensitive]
-          details += " #{sub_key}: '#{left[sub_key]}' != '#{right[sub_key]}'"
-          found = found_one = true
-          @diff_stat[diff_name][sub_key] += 1
         end
-        puts details.gsub(/[\[\]]/,'') if found
+        next if (r_value.is_a?(Array) && '--missing--'.eql?(r_value.first)) || (l_value.is_a?(Array) && '--missing--'.eql?(l_value.first))
+                # TODO: get_field_from_v5_product
+        next if r_value.to_s.eql?(l_value.to_s)
+        next if r_value.to_s.upcase.eql?(l_value.to_s.upcase) && @options[:case_insensitive]
+        details += " #{sub_key}: '#{l_value}' != '#{r_value}'"
+        found = found_one = true
+        @diff_stat[diff_name][sub_key] += 1
       end
+      puts details.gsub(/[\[\]]/,'') if found
     end
-    def key_results_details(key_name, l_keys, r_keys, names)
-      key_name += 'S' unless /S$/.match(key_name)
-      @report <<  "#{key_name}: Found #{l_keys.size} items only in #{@l_base} #{r_keys.size} items only in #{@r_base}, compared #{@diff_stat[key_name][NR_COMPARED]} items"
+
+    def key_results_details(component_name, compare_names, l_keys, r_keys)
+      component_name += 'S' unless /S$/.match(component_name)
+      @report <<  "#{component_name}: Found #{l_keys.size} items only in #{@left.basename} #{r_keys.size} items only in #{@right.basename}, compared #{@diff_stat[component_name][NR_COMPARED]} items"
       keys = r_keys - l_keys
-      head = "#{key_name}: #{(keys).size} keys only in #{@r_base}"
-      puts "#{head}: Keys were #{keys}"
+      head = "#{component_name}: #{(keys).size} keys only in #{@right.basename}"
+      puts "#{head}: Keys were #{keys.size > 10 ? "\n" : ''} #{keys}"
       @report << head
       keys = l_keys - r_keys
-      head = "#{key_name}: #{(keys).size} keys only in #{@l_base}"
-      puts "#{head}: Keys were #{keys}"
+      head = "#{component_name}: #{(keys).size} keys only in #{@left.basename}"
+      puts "#{head}: Keys were #{keys.size > 10 ? "\n" : ''} #{keys}"
       @report << head
-    end
-    def load_file(name)
-      puts "Reading #{name} #{(File.size(name)/1024/1024).to_i} MB. This may take some time"
-      left = XmlSimple.xml_in(IO.read(name))
     end
   end
 end
--- spec/compare_spec.rb
+++ spec/compare_spec.rb
@@ -14,8 +14,10 @@ describe Oddb2xml::CompareV5 do
       @result = Oddb2xml::CompareV5.new(@first, @second).compare
     end
     it 'should return true' do
-      expectecd =  {"ITEMS"=>{"NR_COMPARED"=>2, "GTIN"=>0, "PHAR"=>0, "DSCR"=>1, "PEXF"=>1, "PPUB"=>1, "ATC"=>0, "COMP"=>0, "PKG_SIZE"=>0, "IKSCAT"=>0, "PRODNO"=>0}}
-      expect(@result).to eq(expectecd)
+      expected ={"ITEMS"=>{"NR_COMPARED"=>5, "GTIN"=>0, "PHAR"=>0, "DSCR"=>2,
+                            "PEXF"=>2, "PPUB"=>2, "PHARMATYPE"=>0, "ATC"=>0,
+                            "PKG_SIZE"=>0, "IKSCAT"=>0, "PRODNO"=>0}}
+      expect(@result).to eq(expected)
     end
   end
   context 'when comparing v5' do
@@ -27,12 +29,56 @@ describe Oddb2xml::CompareV5 do
       @result = Oddb2xml::CompareV5.new(@first, @second).compare
     end
     it 'should return true' do
-      expectecd = {"PRODUCTS"=>{"NR_COMPARED"=>1, "PRODNO"=>0, "SALECD"=>0, "DSCR"=>1, "DSCRF"=>0, "ATC"=>0, "SUBSTANCE"=>1},
+      expected = {"PRODUCTS"=>{"NR_COMPARED"=>1, "PRODNO"=>0, "SALECD"=>0, "DSCR"=>1, "DSCRF"=>0, "ATC"=>0, "SUBSTANCE"=>1},
                   "LIMITATIONS"=>{"NR_COMPARED"=>3, "LIMNAMEBAG"=>0, "DSCR"=>0, "DSCRF"=>0, "LIMITATION_PTS"=>1},
                   "ITEMS"=>{"NR_COMPARED"=>4, "PHARMATYPE"=>0, "GTIN"=>0, "SALECD"=>0, "DSCR"=>0, "DSCRF"=>0, "PEXF"=>2, "PPUB"=>2, "PKG_SIZE"=>0,
                             "MEASURE"=>0, "DOSAGE_FORM"=>3, "PRODNO"=>0, "PHAR"=>0, "IKSCAT"=>0}}
-      expect(@result).to eq(expectecd)
+      expect(@result).to eq(expected)
+    end
+  end
+  context 'when comparing v3 with v5' do
+    before(:all) do
+      @first = File.join(Oddb2xml::SpecData, 'artikelstamm_P_010917.xml')
+      @first_n = File.join(Oddb2xml::SpecData, 'artikelstamm_N_010917.xml')
+      @second = File.join(Oddb2xml::SpecData, 'v5_second.xml')
+      expect(File.exist?(@first)).to eq true
+      expect(File.exist?(@first_n)).to eq true
+      expect(File.exist?(@second)).to eq true
+      @result = Oddb2xml::CompareV5.new(@first, @second).compare
+    end
+    it 'should return true' do
+      expected = {"ITEMS"=>{"NR_COMPARED"=>2, "GTIN"=>0, "PHAR"=>0, "DSCR"=>1,
+                            "ATC"=>0, "PEXF"=>2, "PPUB"=>1, "PKG_SIZE"=>0, 
+                            "IKSCAT"=>0, "PRODNO"=>0, "PHARMATYPE"=>0, "SL_ENTRY"=>0,
+                            "LIMITATION"=>0, "LIMITATION_PTS"=>1, "LIMITATION_TEXT"=>1, "LIMNAMEBAG"=>0}}
+      expect(@result).to eq(expected)
+    end
+  end
+  context 'when comparing v5 with v3' do
+    before(:all) do
+      @first = File.join(Oddb2xml::SpecData, 'artikelstamm_P_010917.xml')
+      @first_n = File.join(Oddb2xml::SpecData, 'artikelstamm_N_010917.xml')
+      @second = File.join(Oddb2xml::SpecData, 'v5_second.xml')
+      expect(File.exist?(@first)).to eq true
+      expect(File.exist?(@first_n)).to eq true
+      expect(File.exist?(@second)).to eq true
+      @result = Oddb2xml::CompareV5.new(@second, @first).compare
+    end
+    it 'should return true' do
+      expected_keys = ["DSCR", "DSCRF", 
+                       "GTIN", "IKSCAT", "NR_COMPARED", "PEXF", "PHAR", 
+                       "PHARMATYPE", "PKG_SIZE", "PPUB", "PRODNO", "SALECD", "SL_ENTRY"]
+
+      expect(@result['ITEMS'].keys.sort).to eq expected_keys
+      one_or_more = ["NR_COMPARED", 'PEXF', "SALECD", "DSCR", "DSCRF"]
+      one_or_more.each do |fieldname|
+        binding.pry unless @result['ITEMS'][fieldname] && @result['ITEMS'][fieldname] > 0
+        expect(@result['ITEMS'][fieldname]).to be > 0
+      end
+      (expected_keys-one_or_more).each do |fieldname|
+        expect("#{fieldname} #{@result['ITEMS'][fieldname]}").to eq "#{fieldname} 0"
+      end
+      
     end
   end
 end
-
